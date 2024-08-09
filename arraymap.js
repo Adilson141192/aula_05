410606bed38daf56d546e675199523f2d05de835
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-08-09 12:19:55 -0300

    080924 aula0505

--- arraymap.js
+++ arraymap.js
@@ -15,3 +15,14 @@ var funcionarios = [
 
 nomes = funcionarios.map(pessoa => pessoa.nome);
 console.log(nomes);
+
+var total = 0;
+for (var i = 0; i < numeros.length;i++){
+    total += numeros[i];
+}
+
+console.log(total);
+
+var tot = numeros.reduce(function(total,numero){
+    return total = + numero;
+}, 0);
